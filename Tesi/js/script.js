930ea0b54a5fde7aabf58a3adb43b9f7186f3b7a
GabrieleCicconetti <gab.0@hotmail.it>
2018-04-18 22:49:40 +0200

    Add files via upload

---
+++ Tesi/js/script.js
@@ -0,0 +1,32 @@
+
+
+
+$(window).load(function(){
+
+
+        getRowEsameUtente(function(data){
+            console.log(data)
+        })
+
+
+      $('.dragright').on('dragover', function(e){
+        e.preventDefault()
+      })
+
+      $('.dragright').on('drop', function(){
+        console.log('asdasd')
+      })
+
+
+    function getRowEsameUtente(completion){
+        $.ajax({
+            url: "./template/row-esame-utente.html",
+            async: false,
+            success: function (data){
+                completion(data)
+            }
+        });
+    }
+
+
+})
