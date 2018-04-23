d1ec6f38e5adaffecbfb6944b77ba4f5bf388de2
GabrieleCicconetti <gab.0@hotmail.it>
2018-04-23 14:46:13 +0200

    Create row-esame-facolta.php

---
+++ Tesi/template/row-esame-facolta.php
@@ -0,0 +1,17 @@
+
+<div class="col-xs-12 row-esame-facolta" draggable="true">
+
+    <div class="row">
+
+        <div class="col-xs-10 border-right"><h6 class="text-center"><?=$_GET['nome']?></h6></div>
+        <div class="col-xs-2"><h6 class="text-center"><?=$_GET['cfu']?></h6></div>
+        <div style="clear: both;"></div>
+        <div class="drop-zone" id="<?=$_GET['id']?>-ef">
+            <div class="ghost"></div>
+        </div>
+
+    </div>
+
+
+</div>
+
