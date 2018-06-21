b31458c9d494d3f90cf3f7ffb41e82eae725fc7c
GabrieleCicconetti <gab.0@hotmail.it>
2018-06-21 13:46:11 +0200

    Update row-esame-utente.php

--- Tesi/template/row-esame-utente.php
+++ Tesi/template/row-esame-utente.php
@@ -3,10 +3,10 @@
 
     <div class="row">
 
-        <div class="col-xs-2 border-right text-center"><button class="modifica-esame-utente btn btn-primary"><i class="far fa-edit"></i></button></div>
+        <div class="col-xs-2 border-right text-center"><button class="modifica-esame-utente btn btn-info"><i class="far fa-edit"></i></button></div>
         <div class="col-xs-2 text-center border-right"><button class="elimina-esame-utente btn btn-danger"><i class="far fa-trash-alt"></i></button></div>
         <div class="col-xs-5 nome-esame-utente border-right"><h6 class="text-center"><?=$_GET['nome']?></h6></div>
-        <div class="col-xs-2 cfu-esame-utente border-right"><h6 class="text-center"><?=$_GET['cfu']?></h6></div>
+        <div class="col-xs-2 cfu-esame-utente border-right"><h6 class="text-center">0 / <?=$_GET['cfu']?></h6></div>
         <div class="col-xs-1 center-vertical drag-action"><img draggable="false" src="img/arrow.png" alt=""></div>
 
     </div>
