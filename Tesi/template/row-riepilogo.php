308d3828bcbb1bc1a2eaaf1d533ee58dfff60da7
GabrieleCicconetti <gab.0@hotmail.it>
2018-06-21 13:46:26 +0200

    Update row-riepilogo.php

--- Tesi/template/row-riepilogo.php
+++ Tesi/template/row-riepilogo.php
@@ -1,16 +1,14 @@
+<div class="col-xs-12 row-riepilogo" id="<?=$_GET['id']?>-r">
 
-<div class="col-xs-12 row-esame-utente" draggable="true" id="<?=$_GET['id']?>-eu">
+    <div class="row inner">
 
-    <div class="row">
-
-        <div class="col-xs-2 border-right text-center"><button class="modifica-esame-utente btn btn-primary"><i class="far fa-edit"></i></button></div>
-        <div class="col-xs-2 text-center border-right"><button class="elimina-esame-utente btn btn-danger"><i class="far fa-trash-alt"></i></button></div>
-        <div class="col-xs-5 nome-esame-utente border-right"><h6 class="text-center"><?=$_GET['nome']?></h6></div>
-        <div class="col-xs-2 cfu-esame-utente border-right"><h6 class="text-center"><?=$_GET['cfu']?></h6></div>
-        <div class="col-xs-1 center-vertical drag-action"><img draggable="false" src="img/arrow.png" alt=""></div>
+        <div class="col-xs-3 nome-esame-utente border-right"><h6 class="text-center"><?=$_GET['nome_esame_utente']?></h6></div>
+        <div class="col-xs-2 cfu-esame-utente border-right"><h6 class="text-center"><?=$_GET['cfu_esame_utente']?></h6></div>
+        <div class="col-xs-3 nome-esame-facolta border-right"><h6 class="text-center"><?=$_GET['nome_esame_facolta']?></h6></div>
+        <div class="col-xs-2 cfu-esame-facolta border-right"><h6 class="text-center"><?=$_GET['cfu_esame_facolta']?></h6></div>
+        <div class="col-xs-2 riepilogo-cfu border-right"><h6 class="text-center"><?=$_GET['riepilogo_cfu']?></h6></div>
 
     </div>
 
 
 </div>
-
